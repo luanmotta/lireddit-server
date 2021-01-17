142440726ff3f7b8d0398ee556dd181112aabc45
Luan Motta <luanmtrs@gmail.com>
2021-01-16 23:24:18 -0300

    Creating migrations

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,29 @@
 import { MikroORM } from '@mikro-orm/core';
 import { Post } from './entities/Post';
 import microConfig from './mikro-orm.config';
+import express from 'express';
+import { ApolloServer} from 'apollo-server-express';
+import { buildSchema } from 'type-graphql';
+
+const SERVER_PORT = 4000;
 
 (async () => {
   const orm = await MikroORM.init(microConfig);
-  const post = orm.em.create(Post, { title: 'my first post'});
-  await orm.em.persistAndFlush(post);
+  await orm.getMigrator().up();
+  const app = express();
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [],
+      validate: false,
+    })
+  });
+
+  app.get('/', (_, res) => {
+    res.send('hello');
+  })
+  app.listen(SERVER_PORT, () => {
+    console.log(`Server running on port ${SERVER_PORT}`)
+  })
 })();
 
 console.log('Hello There');
--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -1,8 +1,13 @@
 import { __prod__ } from "./contstants";
 import { Post } from "./entities/Post";
 import { MikroORM } from '@mikro-orm/core';
+import path from "path";
 
 export default {
+  migrations: {
+    path: path.join(__dirname, './migrations'),
+    pattern: /^[\w-]+\d+\.[tj]s$/,
+  },
   entities: [Post],
   user: 'postgres',
   password: 'postgres',
